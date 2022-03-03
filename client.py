bddf34beaf1472220a00fd8f2bdb5700f4544336
beyonsay <73654501+beyonsay@users.noreply.github.com>
2022-03-03 20:00:27 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,14 +1,22 @@
+from ast import While
 from socket import * 
 
-serverName = "hostname"#should be IP address
+serverName = "196.42.95.187"
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-message = input("Input lowercase sentence:")
+message = input("Enter your username:   ")
+
 clientSocket.sendto(message.encode(), (serverName, serverPort))
 
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+#message = input("Who are you chatting to?:  ")
+#clientSocket.sendto(message.encode(), (serverName, serverPort))
 
-print(modifiedMessage.decode())
-clientSocket.close()
+#modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+
+while True:
+    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    print(modifiedMessage.decode())
+    message = input("Write message:     ")
+    clientSocket.sendto(message.encode(), (serverName, serverPort))
